537085f93b8170e9da1cfaa16a8dc4ad0da8ff59
DAUBANAy Etienne <contact@etiennedaubanay.fr>
2021-03-04 23:29:07 +0100

    Ajout de la classe Pasengenr and pakage classe

---
+++ src/classe/Passenger.java
@@ -0,0 +1,13 @@
+public class Passenger {
+    String nom;
+    int age;
+    String reduction;
+    Billet[] mesBillets;
+    public Passenger (int age, String reduction, Billet[] mesBillets) {
+        this.nom = nom;
+        this.age = age;
+        this.reduction = reduction;
+        this.mesBillets = mesBillets;
+    }
+
+}
